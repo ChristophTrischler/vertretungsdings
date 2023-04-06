4349a33609dd71d76f20e9978f121ed6bc3bd691
CHr <christoph.trischler@t-online.de>
2023-04-06 13:40:17 +0200

    formated

--- vertretungsapi/src/main.rs
+++ vertretungsapi/src/main.rs
@@ -1,38 +1,39 @@
-mod vertretundsdings;
 mod check_loop;
+mod vertretundsdings;
 
-use actix_web::{*, web::{Path, Json}};
-use actix_web::web::Data;
 use actix_cors::Cors;
+use actix_web::web::Data;
+use actix_web::{
+    web::{Json, Path},
+    *,
+};
 
-use sqlx::postgres::{PgConnectOptions, PgPoolOptions, PgPool};
-use sqlx::{Row};
+use sqlx::postgres::{PgConnectOptions, PgPool, PgPoolOptions};
+use sqlx::Row;
 use uuid::Uuid;
 
 use std::sync::{Arc, Mutex};
 
-use vertretundsdings::vertretungsdings::{VDay, Plan, get_day, Day};
 use check_loop::init_vday_cache;
+use vertretundsdings::vertretungsdings::{get_day, Day, Plan, VDay};
 
 pub type VdayCache = Mutex<Vec<VDay>>;
 pub type UpdatedList = Mutex<Vec<Uuid>>;
 
-
 #[get("/update/{id}")]
-async fn updated(id: Path<Uuid>, update_list: Data<UpdatedList>) -> impl Responder{
+async fn updated(id: Path<Uuid>, update_list: Data<UpdatedList>) -> impl Responder {
     let mut val = true;
     if let Ok(mut list) = update_list.try_lock() {
         match list.contains(&id) {
             true => val = false,
-            false => list.push(*id)
+            false => list.push(*id),
         }
     }
     HttpResponse::Ok().json(val)
 }
 
-
 #[get("/vdays")]
-async fn get_vdays(vdays: Data<VdayCache>)-> actix_web::Result<impl Responder>{
+async fn get_vdays(vdays: Data<VdayCache>) -> actix_web::Result<impl Responder> {
     let data = vdays.try_lock().unwrap();
     let vdays: &Vec<VDay> = data.as_ref();
     Ok(HttpResponse::Ok().json(&vdays))
@@ -47,22 +48,23 @@ async fn get_days(plan: Json<Plan>, vdays_data: Data<VdayCache>) -> impl Respond
 }
 
 #[get("/days/{plan_id}")]
-async fn get_days_by_plan_id(plan_id: Path<i64>, dbconnection: Data<PgPool>, 
-    vdays_data: Data<VdayCache>) -> impl Responder {
+async fn get_days_by_plan_id(
+    plan_id: Path<i64>,
+    dbconnection: Data<PgPool>,
+    vdays_data: Data<VdayCache>,
+) -> impl Responder {
     let query_result = sqlx::query("SELECT \"data\" FROM \"user\" WHERE \"discord_id\" = $1")
-    .bind(plan_id.as_ref())
-    .fetch_one(dbconnection.as_ref())
-    .await;
-    if let Ok(row) =  query_result{
+        .bind(plan_id.as_ref())
+        .fetch_one(dbconnection.as_ref())
+        .await;
+    if let Ok(row) = query_result {
         let data: String = row.try_get(0).unwrap();
         let plan: Plan = serde_json::from_str(&data).unwrap();
         let vdays = vdays_data.try_lock().unwrap();
         let days: Vec<Day> = vdays.iter().map(|vday| get_day(vday, &plan)).collect();
         HttpResponse::Ok().json(days)
-    }
-    else {
-        HttpResponse::BadRequest()
-        .body("[]")
+    } else {
+        HttpResponse::BadRequest().body("[]")
     }
 }
 
@@ -73,21 +75,20 @@ async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     log::info!("starting HTTP server at http://localhost:8000");
 
-    let (vdays,updated_list, handle, cancel_token)
-        = init_vday_cache();
+    let (vdays, updated_list, handle, cancel_token) = init_vday_cache();
 
     let pg_pool = Arc::new(
         PgPoolOptions::new()
-        .max_connections(15)
-        .connect_with(
-            PgConnectOptions::new()
-            .host("db")
-            .database("vertretungsdings")
-            .username("postgres")
-            .password("pass")
-        )
-        .await
-        .expect("Err creating client")
+            .max_connections(15)
+            .connect_with(
+                PgConnectOptions::new()
+                    .host("db")
+                    .database("vertretungsdings")
+                    .username("postgres")
+                    .password("pass"),
+            )
+            .await
+            .expect("Err creating client"),
     );
 
     HttpServer::new(move || {
@@ -95,11 +96,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(Data::from(Arc::clone(&vdays)))
             .app_data(Data::from(Arc::clone(&updated_list)))
             .app_data(Data::from(Arc::clone(&pg_pool)))
-            .wrap(
-                Cors::default()
-                .allow_any_origin()
-                .allow_any_method() 
-            )
+            .wrap(Cors::default().allow_any_origin().allow_any_method())
             .wrap(middleware::Logger::default())
             .service(get_vdays)
             .service(updated)
@@ -114,3 +111,4 @@ async fn main() -> std::io::Result<()> {
     log::info!("application successfully shut down gracefully");
     Ok(())
 }
+
--- vertretungsapi/src/vertretundsdings/vertretungsdings.rs
+++ vertretungsapi/src/vertretundsdings/vertretungsdings.rs
@@ -1,49 +1,60 @@
-use reqwest::{Client};
-use scraper::{Html, Selector};
-use std::env;
-use serde::{Deserialize, Serialize};
 use chrono::naive::NaiveDate;
+use futures::TryFutureExt;
 use itertools::Itertools;
+use reqwest::Client;
+use scraper::{Html, Selector};
+use serde::{Deserialize, Serialize};
+use std::env;
 
 #[derive(Debug)]
 pub enum ChangeOption<T> {
     Some(T),
     Same(T),
     None,
-    End
+    End,
 }
 
-
-pub async fn check_change(number: i64, last_time :&mut String, last_date: &mut NaiveDate) -> ChangeOption<VDay>{  
-    let url = format!("https://geschuetzt.bszet.de/s-lk-vw/Vertretungsplaene/V_PlanBGy/V_DC_00{}.html",number);
+pub async fn check_change(
+    number: i64,
+    last_time: &mut String,
+    last_date: &mut NaiveDate,
+) -> ChangeOption<VDay> {
+    let url = format!(
+        "https://geschuetzt.bszet.de/s-lk-vw/Vertretungsplaene/V_PlanBGy/V_DC_00{}.html",
+        number
+    );
     let c = Client::new();
-    let result = c.get(url)
+    let result = c
+        .get(url)
         .basic_auth("bsz-et-2223", Some(env::var("PW").expect("no PW in env")))
         .send()
-        .await;
+        .map_ok(|e| {
+            if e.status().is_success() {
+                Option::Some(e)
+            } else {
+                Option::None
+            }
+        })
+        .await
+        .ok()
+        .flatten();
 
-    let res = if let Ok(r) = result {
-        r
-    }else{
-        return ChangeOption::End;
+    let res = match result {
+        Some(r) => r,
+        None => return ChangeOption::End,
     };
-    if !res.status().is_success() {
-        return ChangeOption::End;
-    }
 
     let headers = res.headers();
-    let this_time = headers.get("last-modified")
-    .unwrap()
-    .to_str()
-    .unwrap()
-    .to_string();
+    let this_time = headers
+        .get("last-modified")
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string();
 
-    let text = res.text()
-    .await
-    .unwrap();
+    let text = res.text().await.unwrap();
 
-
-    let vday =  match get_vday(&text, last_date){
+    let vday = match get_vday(&text, last_date) {
         Some(vday) => vday,
         None => return ChangeOption::None,
     };
@@ -57,24 +68,24 @@ pub async fn check_change(number: i64, last_time :&mut String, last_date: &mut N
     };
 }
 
-
-pub fn get_vday(text: &String,last_date: &mut NaiveDate) -> Option<VDay> {
+pub fn get_vday(text: &String, last_date: &mut NaiveDate) -> Option<VDay> {
     let doc = Html::parse_document(text);
 
     let date_selection = Selector::parse(r#"h1[class="list-table-caption"]"#).unwrap();
-    let date = doc.select(&date_selection).next()
-    .unwrap()
-    .inner_html()
-    .trim()
-    .to_string();
+    let date = doc
+        .select(&date_selection)
+        .next()
+        .unwrap()
+        .inner_html()
+        .trim()
+        .to_string();
 
     let date_str = date.split_whitespace().last().unwrap();
     let this_date = NaiveDate::parse_from_str(date_str, "%d.%m.%Y").unwrap();
-    
-    if this_date <= *last_date{
+
+    if this_date <= *last_date {
         return None;
-    } 
-    else {
+    } else {
         *last_date = this_date;
     }
 
@@ -86,93 +97,108 @@ pub fn get_vday(text: &String,last_date: &mut NaiveDate) -> Option<VDay> {
 
     let table = doc.select(&table_body_selection).next().unwrap();
 
-    for row in table.select(&table_row_selection){
+    for row in table.select(&table_row_selection) {
         let fields = row.select(&table_field_selection);
 
-        let mut content_fields:Vec<String> = fields.map(|item| item.inner_html().trim().to_string()).collect();
-        if row.inner_html().contains("&nbsp;"){
-            let last_lesson = v_lessons.last()
-            .unwrap()
-            .to_vec();
-            for (i, s) in &mut content_fields.iter_mut()
-            .enumerate()
-            .filter(|(_i, s)| s.contains("&nbsp;")){
+        let mut content_fields: Vec<String> = fields
+            .map(|item| item.inner_html().trim().to_string())
+            .collect();
+        if row.inner_html().contains("&nbsp;") {
+            let last_lesson = v_lessons.last().unwrap().to_vec();
+            for (i, s) in &mut content_fields
+                .iter_mut()
+                .enumerate()
+                .filter(|(_i, s)| s.contains("&nbsp;"))
+            {
                 let replacement = last_lesson.get(i).unwrap();
                 *s = replacement.to_string();
-            } 
+            }
         }
-    
-        v_lessons.push(Lesson{
+
+        v_lessons.push(Lesson {
             class: content_fields.get(0).unwrap().into(),
-            time: content_fields.get(1).unwrap()
-            .trim_end_matches('.')
-            .parse()
-            .unwrap(),
+            time: content_fields
+                .get(1)
+                .unwrap()
+                .trim_end_matches('.')
+                .parse()
+                .unwrap(),
             subject: content_fields.get(2).unwrap().into(),
             room: content_fields.get(3).unwrap().into(),
             teacher: content_fields.get(4).unwrap().into(),
             vtype: content_fields.get(5).unwrap().into(),
-            message: content_fields.get(6).unwrap().into()
+            message: content_fields.get(6).unwrap().into(),
         });
     }
-    v_lessons = v_lessons.into_iter()
-    .unique_by(Lesson::convert_to_compareable)
-    .collect();
+    v_lessons = v_lessons
+        .into_iter()
+        .unique_by(Lesson::convert_to_compareable)
+        .collect();
 
     return Some(VDay(date, v_lessons.into()));
 }
 
-
-pub fn get_day(VDay(day_str, v_lessons): &VDay, plan :&Plan)->Day{
-    let mut splits =  day_str.split_whitespace().into_iter();
+pub fn get_day(VDay(day_str, v_lessons): &VDay, plan: &Plan) -> Day {
+    let mut splits = day_str.split_whitespace().into_iter();
     let day_name = splits.next().unwrap();
-    let date_str =  splits.next().unwrap();
+    let date_str = splits.next().unwrap();
 
     let date = NaiveDate::parse_from_str(date_str, "%d.%m.%Y").unwrap();
     let ref_date = NaiveDate::from_ymd_opt(2022, 8, 22).unwrap();
     let week = date.signed_duration_since(ref_date).num_weeks() % 2 + 1;
 
-    let mut res_day:Day = Day::new(&day_str.to_string());
+    let mut res_day: Day = Day::new(&day_str.to_string());
 
-    for v_lesson in v_lessons.iter()
-    .filter(|item| item.class.contains(&plan.class_name) && is_in(&item.subject, &plan.subjects)){
-        res_day.lessons.get_mut((v_lesson.time-1) as usize)
-        .unwrap()
-        .push(v_lesson.clone());      
+    for v_lesson in v_lessons.iter().filter(|item| {
+        item.class.contains(&plan.class_name) && is_in(&item.subject, &plan.subjects)
+    }) {
+        res_day
+            .lessons
+            .get_mut((v_lesson.time - 1) as usize)
+            .unwrap()
+            .push(v_lesson.clone());
     }
 
-    let plan_day = plan.days.iter()
-    .find(|item| item.day.contains(day_name))
-    .unwrap();
-    let empty_times = res_day.lessons.iter_mut()
-    .enumerate()
-    .filter(|(i, item)| i%2==0 && item.len()==0);
- 
+    let plan_day = plan
+        .days
+        .iter()
+        .find(|item| item.day.contains(day_name))
+        .unwrap();
+    let empty_times = res_day
+        .lessons
+        .iter_mut()
+        .enumerate()
+        .filter(|(i, item)| i % 2 == 0 && item.len() == 0);
+
     for (i, ls) in empty_times {
-        let normal = plan_day.lessons.get(i/2).unwrap();
+        let normal = plan_day.lessons.get(i / 2).unwrap();
         match normal {
             WeekOption::AandB(l) => ls.push(l.to_lesson()),
-            WeekOption::A(l) => if week == 1 {
-                ls.push(l.to_lesson());
-            },
-            WeekOption::B(l) => if week == 2 {
-                ls.push(l.to_lesson());
-            },
-            WeekOption::AorB(l1, l2) => if week == 1{
-                ls.push(l1.to_lesson());
+            WeekOption::A(l) => {
+                if week == 1 {
+                    ls.push(l.to_lesson());
+                }
             }
-            else {
-                ls.push(l2.to_lesson());
-            },
-            WeekOption::None => (),   
-        }   
-    };
-    return res_day; 
-} 
+            WeekOption::B(l) => {
+                if week == 2 {
+                    ls.push(l.to_lesson());
+                }
+            }
+            WeekOption::AorB(l1, l2) => {
+                if week == 1 {
+                    ls.push(l1.to_lesson());
+                } else {
+                    ls.push(l2.to_lesson());
+                }
+            }
+            WeekOption::None => (),
+        }
+    }
+    return res_day;
+}
 
-
-fn is_in(string: &str, vec: &Vec<String>)->bool{
-    for s in vec{
+fn is_in(string: &str, vec: &Vec<String>) -> bool {
+    for s in vec {
         if string.contains(s) {
             return true;
         }
@@ -181,20 +207,20 @@ fn is_in(string: &str, vec: &Vec<String>)->bool{
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct PlanLesson{
+pub struct PlanLesson {
     time: i64,
     subject: String,
     room: String,
-    teacher: String
+    teacher: String,
 }
 
 impl PlanLesson {
-    pub fn to_lesson(&self)->Lesson{
+    pub fn to_lesson(&self) -> Lesson {
         Lesson::new(
             self.time,
             self.subject.as_str(),
             self.room.as_str(),
-            self.teacher.as_str()
+            self.teacher.as_str(),
         )
     }
 }
@@ -206,38 +232,45 @@ pub struct Lesson {
     pub subject: String,
     pub room: String,
     pub teacher: String,
-    pub vtype: String, 
+    pub vtype: String,
     pub message: String,
 }
 
 impl Lesson {
-    fn new(time: i64, subject: &str, room: &str, teacher: &str)->Lesson{
-        Lesson { 
-            class: String::new(), 
-            time: time, 
-            subject: subject.to_string(), 
-            room: room.to_string(), 
-            teacher: teacher.to_string(), 
-            vtype: String::new(), 
-            message: String::new() 
+    fn new(time: i64, subject: &str, room: &str, teacher: &str) -> Lesson {
+        Lesson {
+            class: String::new(),
+            time: time,
+            subject: subject.to_string(),
+            room: room.to_string(),
+            teacher: teacher.to_string(),
+            vtype: String::new(),
+            message: String::new(),
         }
     }
 
-    fn convert_to_compareable(&self) -> (String, String, String, String, String, String, i32){
+    fn convert_to_compareable(&self) -> (String, String, String, String, String, String, i32) {
         (
             self.class.to_string(),
             self.subject.to_string(),
             self.room.to_string(),
             self.teacher.to_string(),
-            self.vtype.to_string(), 
+            self.vtype.to_string(),
             self.message.to_string(),
-            ((self.time + 1) / 2) as i32
+            ((self.time + 1) / 2) as i32,
         )
     }
 
-    fn to_vec(&self) -> Vec<String>{
-        vec![self.class.to_string(), self.time.to_string(), self.subject.to_string(), 
-        self.room.to_string(), self.teacher.to_string(), self.vtype.to_string(), self.message.to_string()]
+    fn to_vec(&self) -> Vec<String> {
+        vec![
+            self.class.to_string(),
+            self.time.to_string(),
+            self.subject.to_string(),
+            self.room.to_string(),
+            self.teacher.to_string(),
+            self.vtype.to_string(),
+            self.message.to_string(),
+        ]
     }
 }
 
@@ -245,7 +278,7 @@ impl Lesson {
 pub struct Plan {
     pub class_name: String,
     pub days: Vec<PlanDay>,
-    pub subjects: Vec<String>
+    pub subjects: Vec<String>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
@@ -259,25 +292,26 @@ pub enum WeekOption {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
-pub struct PlanDay{
-    pub day: String, 
-    pub lessons: [WeekOption; 5]
-} 
+pub struct PlanDay {
+    pub day: String,
+    pub lessons: [WeekOption; 5],
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct VDay(String, Vec<Lesson>);
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Day {  
+pub struct Day {
     pub day: String,
-    pub lessons: [Vec<Lesson>; 10]
+    pub lessons: [Vec<Lesson>; 10],
 }
 
 impl Day {
-    pub fn new(day: &str) -> Day{
+    pub fn new(day: &str) -> Day {
         Day {
             day: day.to_string(),
-            lessons: Default::default()
+            lessons: Default::default(),
         }
     }
 }
+
